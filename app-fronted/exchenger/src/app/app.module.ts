b1cd98141a10e072ef40083d0e52f84b5d11a094
Anatoly Kovalenko <lom.g.lom@gmail.com>
2020-10-09 16:46:11 +0300

    Added Angular in project

---
+++ app-fronted/exchenger/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
